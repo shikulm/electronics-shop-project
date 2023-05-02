edbb82634a62df55fe13f2310e7e1fde240ee64c
Михаил Шикульский <shikul_m@mail.ru>
2023-05-02 14:35:46 +0400

    add __repr__() for class Phone

--- src/phone.py
+++ src/phone.py
@@ -19,7 +19,8 @@ class Phone(Item):
             self.__number_of_sim = number_of_sim
 
 
-
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
 
 
 
